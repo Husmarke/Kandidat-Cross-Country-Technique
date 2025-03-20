ef21b7b49e4e20f5a7e3d4fa95f5c4e9db0ecd80
Lucasgust1 <150360976+Lucasgust1@users.noreply.github.com>
2025-03-20 15:44:55 +0100

    Code including second foot
    Added pins, reading and output printing for foot 2

--- Arduino/SensorTesting/multiple_loadcells_BT_test/lucas_load_cell/lucas_load_cell.ino
+++ Arduino/SensorTesting/multiple_loadcells_BT_test/lucas_load_cell/lucas_load_cell.ino
@@ -1,5 +1,6 @@
-#include <SoftwareSerial.h>
+   #include <SoftwareSerial.h>
 
+//foot 1
 int analogPin1 = A1; 
 int analogPin2 = A2; 
 int analogPin3 = A3; 
@@ -8,38 +9,61 @@ int val1 = 0;  // variable to store the value read
 int val2 = 0;  // variable to store the value read
 int val3 = 0;  // variable to store the value read
 
+//foot 2
+int analogPin4 = A4;
+int analogPin5 = A5;
+int analogPin6 = A6;
+
+int val4 = 0;  // variable to store the value read
+int val5 = 0;  // variable to store the value read
+int val6 = 0;  // variable to store the value read
+
+
+// Var for first sensor reading
 int zero1 = 0;
 int zero2 = 0;
 int zero3 = 0;
+
+int zero4 = 0;
+int zero5 = 0;
+int zero6 = 0;
 
 // Ultrasonic
 const int trigPin = 2;  
 const int echoPin = 3;
 float dur, dist;
- 
+
 SoftwareSerial BTSerial(2, 3); // RX, TX (Bluetooth module)
   
   void setup() {
     Serial.begin(9600);           //  setup serial
     BTSerial.begin(9600);   // Bluetooth module baud rate
 
-    zero1 = analogRead(analogPin1);  // read the input pin
-    zero2 = analogRead(analogPin2);  // read the input pin
-    zero3 = analogRead(analogPin3);  // read the input pin
+    // read first sensor input
+    zero1 = analogRead(analogPin1);
+    zero2 = analogRead(analogPin2);  
+    zero3 = analogRead(analogPin3);  
 
+    zero4 = analogRead(analogPin4);  
+    zero5 = analogRead(analogPin5);  
+    zero6 = analogRead(analogPin6);  
     
     pinMode(trigPin, OUTPUT);  
     pinMode(echoPin, INPUT);  
     Serial.begin(9600);  
 
   }
-
+  
   void loop() {
     val1 = analogRead(analogPin1)-zero1;  // read the input pin
     val2 = analogRead(analogPin2)-zero2;  // read the input pin
     val3 = analogRead(analogPin3)-zero3;  // read the input pin
 
-    //Echo
+    val4 = analogRead(analogPin4)-zero4;  // read the input pin
+    val5 = analogRead(analogPin5)-zero5;  // read the input pin
+    val6 = analogRead(analogPin6)-zero6;  // read the input pin
+    
+    //Read distance
     digitalWrite(trigPin, LOW);  
     delayMicroseconds(2);  
     digitalWrite(trigPin, HIGH);  
@@ -47,13 +71,11 @@ SoftwareSerial BTSerial(2, 3); // RX, TX (Bluetooth module)
     digitalWrite(trigPin, LOW); 
     dur = pulseIn(echoPin, HIGH); //measure echo duration
     dist = (dur*.0343)/2; //calc distance
-    Serial.print("Distance: ");  
-    Serial.println(dist); 
 
-    String data = "Loadcell_1 " + String(val1) + " Loadcell_2 " + String(val2) +" Loadcell_3 " + String(val3) + "Distance" + String(dist);
+    String data ="Foot_1:\tLoadcell_1 " + String(val1) + " \tLoadcell_2 " + String(val2) +"\tLoadcell_3 " + String(val3) + "\nFoot_2:\tLoadcell_1 " + String(val4) + "\tLoadcell_2 " + String(val5) +"\tLoadcell_3 " + String(val6) + "\nDistance\t" + String(dist);
     Serial.println(data);          // debug value
-
     
+    //"Distance" + String(dist);
     BTSerial.println(data);    // Send data via Bluetooth
     delay(50);
     
