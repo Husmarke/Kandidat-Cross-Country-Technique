a0fe2b04b81e817a1ade54dfd94623cea8665078
Lucasgust1 <150360976+Lucasgust1@users.noreply.github.com>
2025-04-25 11:42:25 +0200

    updated for all sensors, not bluethooth

--- Arduino/FinalCodeV3/FinalCodeV3.ino
+++ Arduino/FinalCodeV3/FinalCodeV3.ino
@@ -11,71 +11,85 @@
 // white = second
 // silver = third
 
-const int LoadcellPins[NUM_SOURCES] = {A5,A7,A6,A10,A9,A8};
-const int IMUPins[NUM_SOURCES] = {A1,A2,A3};
+const int LoadcellPins[NUM_SOURCES] = {A5, A12, A6, A10, A9, A8};
+
+#define TRIG_PIN 4 // remove when getdistance has been changed
+#define ECHO_PIN 5
 
 // LIDAR sensor
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 
-//IMU
-float ax, ay, az;
+//IMU axis(1 for first IMU, 2 for second IMU)
+float ax1, ay1, az1;
+float ax2, ay2, az2;
 
 #define delayMS 5  // Lower delay for higher frequency
 
 // Use SoftwareSerial only if you must – prefer HardwareSerial
 //SoftwareSerial BTSerial(6, 7);  // RX, TX
 
-// Define Software I2C connections (SDA, SCL)(IMU)
-SoftwareWire wire1(A2, 21); // IMU 1: SDA = A2, SCL = D21
-SoftwareWire wire2(A3, 21); // IMU 2: SDA = A3, SCL = D21
-SoftwareWire wire3(A4, 21); // IMU 3: SDA = A4, SCL = D21
-
 // Initialize IMU instances on each software I2C bus(IMU)
-ICM20600 imu1(&wire1);
-ICM20600 imu2(&wire2);
-ICM20600 imu3(&wire3);
+ICM20600 imu1(true); // default
+ICM20600 imu2(false); // changed adress
 
 
 // ------------------ STATE VARIABLES ------------------
 int strainValues[NUM_SOURCES];
-//float calibrationFactors[NUM_SOURCES] = {0};
+float max_val[NUM_SOURCES] = {0};
+float min_val[NUM_SOURCES] = {10000};
 
 // ---------------------- SETUP ------------------------
 void setup() {
   Serial.begin(9600);    // Faster baud rate
   //BTSerial.begin(9600); /uncomment when BT sensor works and is connected
 
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+    delay(1000);
+  Serial.println("Hello from Arduino!");
   if (!lox.begin()) { // boot check for LIDAR
     Serial.println(F("Failed to boot VL53L0X"));
-
-  //calibrateSensors();  // uncomment when function is changed (see function)
   }
-  wire1.begin();
-  wire2.begin();
-  wire3.begin();
+  
+  calibrateSensors();  // uncomment when function is changed (see function)
+  Wire.begin();
+  Wire.begin();
 
   // Initialize each IMU
   imu1.initialize();
   imu2.initialize();
-  imu3.initialize();
+
+
 }
 
 // ------------------ CALIBRATION --------------------
-void calibrateSensors() {// TODO change calibration to use min/max normalization
-  const int samples = 200;
-  float sums[NUM_SOURCES] = {0};
 
+void calibrateSensors() {// TODO change calibration to use min/max normalization
+  //BTSerial.print("Calibration loadcells, move and jump");
+  Serial.print("Calibration loadcells, move and jump");
+  
+  const int samples = 500;
+  float cur_val;
+  
   for (int j = 0; j < samples; j++) {
     for (int i = 0; i < NUM_SOURCES; i++) {
-      sums[i] += analogRead(LoadcellPins[i]);
+      cur_val = analogRead(LoadcellPins[i]);
+      if (cur_val > max_val[i]){
+        max_val[i] = cur_val;
+      }
+      else if (cur_val < min_val[i])
+        min_val[i] = cur_val;
     }
     delay(5);  // Small delay to let ADC settle
   }
+  //BTSerial.print("Calabration completed, lets go skiing!");
+  Serial.print("Calibration completed, lets go skiing!");
 }
 
 // ------------------ DISTANCE ------------------
-float getDistance() {
+float getDistance() { // TODO change to work with LIDAR
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
@@ -84,51 +98,65 @@ float getDistance() {
 }
 
 // ------------------ IMU acceleration------------------
-//void getAcceleration(){
-//  float acc[9] = {imu1.getAccelerationX(), imu1.getAccelerationY(), imu1.getAccelerationZ(), imu2.getAccelerationX(), imu2.getAccelerationY(), imu2.getAccelerationZ(), imu3.getAccelerationX(), imu3.getAccelerationY() , imu3.getAccelerationZ()};
-//  return acc;
-//  }
-void getAcceleration(float& ax, float& ay, float& az) {
+void getAcceleration(float& ax1, float& ay1, float& az1, float& ax2, float& ay2, float& az2) {
+ 
+  ax1 = imu1.getAccelerationX();
+  ay1 = imu1.getAccelerationY();
+  az1 = imu1.getAccelerationZ();
   
-  ax = imu1.getAccelerationX();
-  ay = imu1.getAccelerationY();
-  az = imu1.getAccelerationZ();
+  ax2 = imu2.getAccelerationX();
+  ay2 = imu2.getAccelerationY();
+  az2 = imu2.getAccelerationZ();
 }
-
-
+  
+float gx;
 // ------------------ MAIN LOOP ------------------
 void loop() {
   // Read sensors
   for (int i = 0; i < NUM_SOURCES; i++) {
-    strainValues[i] = analogRead(LoadcellPins[i]);
+    strainValues[i] = (analogRead(LoadcellPins[i]) - min_val[i]) / (max_val[i] - min_val[i]) * 100;
   }
 
   float distance = getDistance(); // getting distance in mm from LIDAR
-  getAcceleration(ax, ay, az);
+  getAcceleration(ax1, ay1, az1, ax2, ay2, az2); // getting acceleration from both IMUs
   // Send data using raw print for speed
-  Serial.print("L:");
+
+/* Serial.print("L:");
   for (int i = 0; i < 3; i++) {
     Serial.print(strainValues[i]);
     Serial.print(",");
   }
+  
+  
   Serial.print("R:");
   for (int i = 3; i < 6; i++) {
     Serial.print(strainValues[i]);
     Serial.print(",");
   }
+
+  
   Serial.print("D:");
   Serial.print(distance, 1);
+*/
 
   Serial.print("Acc 1x:");
-  Serial.print(ax, 1);
+  Serial.print(ax1, 1);
 
   Serial.print("Acc 1y:");
-  Serial.print(ay, 1);
+  Serial.print(ay1, 1);
 
   Serial.print("Acc 1z:");
-  Serial.println(az, 1);
+  Serial.println(az1, 1);
 
+  Serial.println("Acc 2x:");
+  Serial.print(ax2, 1);
 
+  Serial.print("Acc 2y:");
+  Serial.print(ay2, 1);
+
+  Serial.print("Acc 2z:");
+  Serial.println(az2, 1);
+  
   // BTSerial follows same structure
   //BTSerial.print("L:");
   //for (int i = 0; i < 3; i++) {
@@ -142,6 +170,6 @@ void loop() {
   //}
   //BTSerial.print("D:");
   //BTSerial.println(distance, 1);
-
+  delay(1000);
   delay(delayMS);  // Tweak or remove to increase rate
 }
